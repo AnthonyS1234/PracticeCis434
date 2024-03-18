f6db17a562c008b23624d5112d8e72836da7e502
AnthonyS1234 <163884235+AnthonyS1234@users.noreply.github.com>
2024-03-18 12:04:24 -0400

    Add files via upload
    Add a class to say hello world.

---
+++ CalculateResults.java
@@ -0,0 +1,14 @@
+package hwjunit;
+
+class CalculateResults {
+
+		double calculateResult (double value,int stop )
+		{
+			double nextValue=0;
+			nextValue=value*stop;
+			return nextValue;
+			
+	}
+
+}
+
